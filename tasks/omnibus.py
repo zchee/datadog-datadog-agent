d8014f7eb6122ce3d6ff066e2e8abc502cf49611
Hugo Beauzée-Luyssen <hugo.beauzee@datadoghq.com>
2023-10-25 13:50:46 +0200

    fix_multi_overrides

--- tasks/omnibus.py
+++ tasks/omnibus.py
@@ -24,9 +24,12 @@ def omnibus_run_task(
     with ctx.cd("omnibus"):
         overrides_cmd = ""
         if base_dir:
-            overrides_cmd = f"--override=base_dir:{base_dir}"
+            overrides_cmd = f"base_dir:{base_dir}"
         if host_distribution:
-            overrides_cmd += f" --override=host_distribution:{host_distribution}"
+            overrides_cmd += f" host_distribution:{host_distribution}"
+        overrides_cmd += ' host:x86_64-unknown-linux-gnu'
+        if overrides_cmd:
+            overrides_cmd = f'--override {overrides_cmd}'
 
         omnibus = "bundle exec omnibus"
         if sys.platform == 'win32':
@@ -41,7 +44,7 @@ def omnibus_run_task(
         else:
             populate_s3_cache = ""
 
-        cmd = "{omnibus} {task} {project_name} --log-level={log_level} {populate_s3_cache} {overrides} --override=host:x86_64-unknown-linux-gnu"
+        cmd = "{omnibus} {task} {project_name} --log-level={log_level} {populate_s3_cache} {overrides}"
         args = {
             "omnibus": omnibus,
             "task": task,
