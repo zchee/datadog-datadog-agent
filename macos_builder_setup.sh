c1029fdce8884a7db4835e66563be5b27e4107a5
Alexandre Menasria <alexandre.menasria@datadoghq.com>
2024-01-29 13:26:12 +0100

    add macos_builder_setup

---
+++ macos_builder_setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Unless explicitly stated otherwise all files in this repository are licensed
+# under the Apache License Version 2.0.
+# This product includes software developed at Datadog (https://www.datadoghq.com/)
+# Copyright 2022-present Datadog, Inc.
+
+set -e
+
+export IBM_MQ_VERSION=9.2.4.0-IBM-MQ-DevToolkit
+
+# Install IBM MQ
+sudo mkdir -p /opt/mqm
+curl --retry 5 --fail "https://s3.amazonaws.com/dd-agent-omnibus/ibm-mq-backup/${IBM_MQ_VERSION}-MacX64.pkg" -o /tmp/mq_client.pkg
+sudo installer -pkg /tmp/mq_client.pkg -target /
+sudo rm -rf /tmp/mq_client.pkg
