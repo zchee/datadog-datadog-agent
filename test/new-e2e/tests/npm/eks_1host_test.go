c8b626b3552c2e220861bdfaae6ed916c32a2de2
Nicolas PLANEL <nicolas.planel@datadoghq.com>
2024-05-17 14:00:28 +0200

    fixup

--- test/new-e2e/tests/npm/eks_1host_test.go
+++ test/new-e2e/tests/npm/eks_1host_test.go
@@ -58,7 +58,7 @@ func eksHttpbinEnvProvisioner() e2e.PulumiEnvRunFunc[eksHttpbinEnv] {
 		}
 
 		// install docker.io
-		manager, _, err := docker.NewManager(&awsEnv, httpbinHost)
+		manager, err := docker.NewManager(&awsEnv, httpbinHost)
 		if err != nil {
 			return err
 		}
