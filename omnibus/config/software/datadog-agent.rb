5de062cbdd5dae87f49f254e9f0b8f786345059d
Kylian Serrania <kylian.serrania@datadoghq.com>
2024-06-07 12:54:24 +0200

    [omnibus] Try not using PathFetcher

--- omnibus/config/software/datadog-agent.rb
+++ omnibus/config/software/datadog-agent.rb
@@ -19,9 +19,6 @@ dependency "openscap" if linux_target? and !arm7l_target? and !heroku_target? # 
 dependency 'agent-dependencies'
 dependency 'datadog-agent-dependencies'
 
-source path: '..'
-relative_path 'src/github.com/DataDog/datadog-agent'
-
 always_build true
 
 build do
@@ -33,11 +30,12 @@ build do
   end
 
   # set GOPATH on the omnibus source dir for this software
-  gopath = Pathname.new(project_dir) + '../../../..'
+  source_path = (Omnibus::Config.project_root + '/..').to_path
   if windows_target?
+
     env = {
-        'GOPATH' => gopath.to_path,
-        'PATH' => "#{gopath.to_path}/bin:#{ENV['PATH']}",
+        'GOPATH' => source_path,
+        'PATH' => "#{source_path}/bin:#{ENV['PATH']}",
         "Python2_ROOT_DIR" => "#{windows_safe_path(python_2_embedded)}",
         "Python3_ROOT_DIR" => "#{windows_safe_path(python_3_embedded)}",
         "CMAKE_INSTALL_PREFIX" => "#{windows_safe_path(python_2_embedded)}",
@@ -47,8 +45,8 @@ build do
     flavor_arg = "%AGENT_FLAVOR%"
   else
     env = {
-        'GOPATH' => gopath.to_path,
-        'PATH' => "#{gopath.to_path}/bin:#{ENV['PATH']}",
+        'GOPATH' => source_path,
+        'PATH' => "#{source_path}/bin:#{ENV['PATH']}",
         "Python2_ROOT_DIR" => "#{install_dir}/embedded",
         "Python3_ROOT_DIR" => "#{install_dir}/embedded",
         "LDFLAGS" => "-Wl,-rpath,#{install_dir}/embedded/lib -L#{install_dir}/embedded/lib",
@@ -75,25 +73,25 @@ build do
     if not windows_arch_i386? and ENV['WINDOWS_DDNPM_DRIVER'] and not ENV['WINDOWS_DDNPM_DRIVER'].empty?
       do_windows_sysprobe = "--windows-sysprobe"
     end
-    command "inv -e rtloader.clean"
-    command "inv -e rtloader.make --python-runtimes #{py_runtimes_arg} --install-prefix \"#{windows_safe_path(python_2_embedded)}\" --cmake-options \"-G \\\"Unix Makefiles\\\"\"", :env => env
-    command "mv rtloader/bin/*.dll  #{install_dir}/bin/agent/"
-    command "inv -e agent.build --exclude-rtloader --python-runtimes #{py_runtimes_arg} --major-version #{major_version_arg} --rebuild --no-development --install-path=#{install_dir} --embedded-path=#{install_dir}/embedded #{do_windows_sysprobe} --flavor #{flavor_arg}", env: env
-    command "inv -e systray.build --major-version #{major_version_arg} --rebuild", env: env
+    command "inv -e rtloader.clean", cwd: source_path
+    command "inv -e rtloader.make --python-runtimes #{py_runtimes_arg} --install-prefix \"#{windows_safe_path(python_2_embedded)}\" --cmake-options \"-G \\\"Unix Makefiles\\\"\"", :env => env, cwd: source_path
+    command "mv rtloader/bin/*.dll  #{install_dir}/bin/agent/", cwd: source_path
+    command "inv -e agent.build --exclude-rtloader --python-runtimes #{py_runtimes_arg} --major-version #{major_version_arg} --rebuild --no-development --install-path=#{install_dir} --embedded-path=#{install_dir}/embedded #{do_windows_sysprobe} --flavor #{flavor_arg}", env: env, cwd: source_path
+    command "inv -e systray.build --major-version #{major_version_arg} --rebuild", env: env, cwd: source_path
   else
-    command "inv -e rtloader.clean"
-    command "inv -e rtloader.make --python-runtimes #{py_runtimes_arg} --install-prefix \"#{install_dir}/embedded\" --cmake-options '-DCMAKE_CXX_FLAGS:=\"-D_GLIBCXX_USE_CXX11_ABI=0 -I#{install_dir}/embedded/include\" -DCMAKE_C_FLAGS:=\"-I#{install_dir}/embedded/include\" -DCMAKE_INSTALL_LIBDIR=lib -DCMAKE_FIND_FRAMEWORK:STRING=NEVER'", :env => env
-    command "inv -e rtloader.install"
+    command "inv -e rtloader.clean", cwd: source_path
+    command "inv -e rtloader.make --python-runtimes #{py_runtimes_arg} --install-prefix \"#{install_dir}/embedded\" --cmake-options '-DCMAKE_CXX_FLAGS:=\"-D_GLIBCXX_USE_CXX11_ABI=0 -I#{install_dir}/embedded/include\" -DCMAKE_C_FLAGS:=\"-I#{install_dir}/embedded/include\" -DCMAKE_INSTALL_LIBDIR=lib -DCMAKE_FIND_FRAMEWORK:STRING=NEVER'", :env => env, cwd: source_path
+    command "inv -e rtloader.install", cwd: source_path
     bundle_arg = bundled_agents ? bundled_agents.map { |k| "--bundle #{k}" }.join(" ") : "--bundle agent"
 
     include_sds = ""
     if linux_target?
         include_sds = "--include-sds" # we only support SDS on Linux targets for now
     end
-    command "inv -e agent.build --exclude-rtloader #{include_sds} --python-runtimes #{py_runtimes_arg} --major-version #{major_version_arg} --rebuild --no-development --install-path=#{install_dir} --embedded-path=#{install_dir}/embedded --python-home-2=#{install_dir}/embedded --python-home-3=#{install_dir}/embedded --flavor #{flavor_arg} #{bundle_arg}", env: env
+    command "inv -e agent.build --exclude-rtloader #{include_sds} --python-runtimes #{py_runtimes_arg} --major-version #{major_version_arg} --rebuild --no-development --install-path=#{install_dir} --embedded-path=#{install_dir}/embedded --python-home-2=#{install_dir}/embedded --python-home-3=#{install_dir}/embedded --flavor #{flavor_arg} #{bundle_arg}", env: env, cwd: source_path
 
     if heroku_target?
-      command "inv -e agent.build --exclude-rtloader --python-runtimes #{py_runtimes_arg} --major-version #{major_version_arg} --rebuild --no-development --install-path=#{install_dir} --embedded-path=#{install_dir}/embedded --python-home-2=#{install_dir}/embedded --python-home-3=#{install_dir}/embedded --flavor #{flavor_arg} --agent-bin=bin/agent/core-agent --bundle agent", env: env
+      command "inv -e agent.build --exclude-rtloader --python-runtimes #{py_runtimes_arg} --major-version #{major_version_arg} --rebuild --no-development --install-path=#{install_dir} --embedded-path=#{install_dir}/embedded --python-home-2=#{install_dir}/embedded --python-home-3=#{install_dir}/embedded --flavor #{flavor_arg} --agent-bin=bin/agent/core-agent --bundle agent", env: env, cwd: source_path
     end
   end
 
@@ -110,38 +108,38 @@ build do
   end
 
   # move around bin and config files
-  move 'bin/agent/dist/datadog.yaml', "#{conf_dir}/datadog.yaml.example"
-  move 'bin/agent/dist/conf.d', "#{conf_dir}/"
+  move 'bin/agent/dist/datadog.yaml', "#{conf_dir}/datadog.yaml.example", cwd: source_path
+  move 'bin/agent/dist/conf.d', "#{conf_dir}/", cwd: source_path
 
   unless windows_target?
-    copy 'bin/agent', "#{install_dir}/bin/"
+    copy 'bin/agent', "#{install_dir}/bin/", cwd: source_path
   else
-    copy 'bin/agent/ddtray.exe', "#{install_dir}/bin/agent"
-    copy 'bin/agent/agent.exe', "#{install_dir}/bin/agent"
-    copy 'bin/agent/dist', "#{install_dir}/bin/agent"
+    copy 'bin/agent/ddtray.exe', "#{install_dir}/bin/agent", cwd: source_path
+    copy 'bin/agent/agent.exe', "#{install_dir}/bin/agent", cwd: source_path
+    copy 'bin/agent/dist', "#{install_dir}/bin/agent", cwd: source_path
     mkdir Omnibus::Config.package_dir() unless Dir.exists?(Omnibus::Config.package_dir())
   end
 
   if not bundled_agents.include? "trace-agent"
     platform = windows_arch_i386? ? "x86" : "x64"
-    command "invoke trace-agent.build --python-runtimes #{py_runtimes_arg} --install-path=#{install_dir} --major-version #{major_version_arg} --flavor #{flavor_arg}", :env => env
+    command "invoke trace-agent.build --python-runtimes #{py_runtimes_arg} --install-path=#{install_dir} --major-version #{major_version_arg} --flavor #{flavor_arg}", :env => env, cwd: source_path
   end
 
   if windows_target?
-    copy 'bin/trace-agent/trace-agent.exe', "#{install_dir}/bin/agent"
+    copy 'bin/trace-agent/trace-agent.exe', "#{install_dir}/bin/agent", cwd: source_path
   else
-    copy 'bin/trace-agent/trace-agent', "#{install_dir}/embedded/bin"
+    copy 'bin/trace-agent/trace-agent', "#{install_dir}/embedded/bin", cwd: source_path
   end
 
   # Process agent
   if not bundled_agents.include? "process-agent"
-    command "invoke -e process-agent.build --python-runtimes #{py_runtimes_arg} --install-path=#{install_dir} --major-version #{major_version_arg} --flavor #{flavor_arg} --no-bundle", :env => env
+    command "invoke -e process-agent.build --python-runtimes #{py_runtimes_arg} --install-path=#{install_dir} --major-version #{major_version_arg} --flavor #{flavor_arg} --no-bundle", :env => env, cwd: source_path
   end
 
   if windows_target?
-    copy 'bin/process-agent/process-agent.exe', "#{install_dir}/bin/agent"
+    copy 'bin/process-agent/process-agent.exe', "#{install_dir}/bin/agent", cwd: source_path
   else
-    copy 'bin/process-agent/process-agent', "#{install_dir}/embedded/bin"
+    copy 'bin/process-agent/process-agent', "#{install_dir}/embedded/bin", cwd: source_path
   end
 
   # System-probe
@@ -149,46 +147,46 @@ build do
   if sysprobe_support
     if not bundled_agents.include? "system-probe"
       if windows_target?
-        command "invoke -e system-probe.build"
+        command "invoke -e system-probe.build", cwd: source_path
       elsif linux_target?
-        command "invoke -e system-probe.build-sysprobe-binary --install-path=#{install_dir} --no-bundle"
+        command "invoke -e system-probe.build-sysprobe-binary --install-path=#{install_dir} --no-bundle", cwd: source_path
       end
     end
 
     if windows_target?
-      copy 'bin/system-probe/system-probe.exe', "#{install_dir}/bin/agent"
+      copy 'bin/system-probe/system-probe.exe', "#{install_dir}/bin/agent", cwd: source_path
     elsif linux_target?
-      copy "bin/system-probe/system-probe", "#{install_dir}/embedded/bin"
+      copy "bin/system-probe/system-probe", "#{install_dir}/embedded/bin", cwd: source_path
     end
 
     # Add SELinux policy for system-probe
     if debian_target? || redhat_target?
       mkdir "#{conf_dir}/selinux"
-      command "inv -e selinux.compile-system-probe-policy-file --output-directory #{conf_dir}/selinux", env: env
+      command "inv -e selinux.compile-system-probe-policy-file --output-directory #{conf_dir}/selinux", env: env, cwd: source_path
     end
 
-    move 'bin/agent/dist/system-probe.yaml', "#{conf_dir}/system-probe.yaml.example"
+    move 'bin/agent/dist/system-probe.yaml', "#{conf_dir}/system-probe.yaml.example", cwd: source_path
   end
 
   # Security agent
   secagent_support = (not heroku_target?) and (not windows_target? or (ENV['WINDOWS_DDPROCMON_DRIVER'] and not ENV['WINDOWS_DDPROCMON_DRIVER'].empty?))
   if secagent_support
     if not bundled_agents.include? "security-agent"
-      command "invoke -e security-agent.build --install-path=#{install_dir} --major-version #{major_version_arg} --no-bundle", :env => env
+      command "invoke -e security-agent.build --install-path=#{install_dir} --major-version #{major_version_arg} --no-bundle", :env => env, cwd: source_path
     end
     if windows_target?
-      copy 'bin/security-agent/security-agent.exe', "#{install_dir}/bin/agent"
+      copy 'bin/security-agent/security-agent.exe', "#{install_dir}/bin/agent", cwd: source_path
     else
-      copy 'bin/security-agent/security-agent', "#{install_dir}/embedded/bin"
+      copy 'bin/security-agent/security-agent', "#{install_dir}/embedded/bin", cwd: source_path
     end
-    move 'bin/agent/dist/security-agent.yaml', "#{conf_dir}/security-agent.yaml.example"
+    move 'bin/agent/dist/security-agent.yaml', "#{conf_dir}/security-agent.yaml.example", cwd: source_path
   end
 
   # APM Injection agent
   if windows_target?
     if ENV['WINDOWS_APMINJECT_MODULE'] and not ENV['WINDOWS_APMINJECT_MODULE'].empty?
-      command "inv agent.generate-config --build-type apm-injection --output-file ./bin/agent/dist/apm-inject.yaml", :env => env
-      move 'bin/agent/dist/apm-inject.yaml', "#{conf_dir}/apm-inject.yaml.example"
+      command "inv agent.generate-config --build-type apm-injection --output-file ./bin/agent/dist/apm-inject.yaml", :env => env, cwd: source_path
+      move 'bin/agent/dist/apm-inject.yaml', "#{conf_dir}/apm-inject.yaml.example", cwd: source_path
     end
   end
 
@@ -206,12 +204,12 @@ build do
     # Systray GUI
     app_temp_dir = "#{install_dir}/Datadog Agent.app/Contents"
     mkdir "#{app_temp_dir}/MacOS"
-    systray_build_dir = "#{project_dir}/comp/core/gui/guiimpl/systray"
+    systray_build_dir = "#{source_path}/comp/core/gui/guiimpl/systray"
     # Target OSX 10.10 (it brings significant changes to Cocoa and Foundation APIs, and older versions of OSX are EOL'ed)
     # Add @executable_path/../Frameworks to rpath to find the swift libs in the Frameworks folder.
     command 'swiftc -O -swift-version "5" -target "x86_64-apple-macosx10.10" -Xlinker \'-rpath\' -Xlinker \'@executable_path/../Frameworks\' Sources/*.swift -o gui', cwd: systray_build_dir
-    copy "#{systray_build_dir}/gui", "#{app_temp_dir}/MacOS/"
-    copy "#{systray_build_dir}/agent.png", "#{app_temp_dir}/MacOS/"
+    copy "#{systray_build_dir}/gui", "#{app_temp_dir}/MacOS/", cwd: source_path
+    copy "#{systray_build_dir}/agent.png", "#{app_temp_dir}/MacOS/", cwd: source_path
   end
 
   # The file below is touched by software builds that don't put anything in the installation
--- omnibus/config/software/system-probe.rb
+++ omnibus/config/software/system-probe.rb
@@ -5,9 +5,6 @@
 
 name 'system-probe'
 
-source path: '..'
-relative_path 'src/github.com/DataDog/datadog-agent'
-
 always_build true
 
 build do
@@ -22,14 +19,14 @@ build do
   mkdir "#{install_dir}/embedded/share/system-probe/java"
 
   if ENV.has_key?('SYSTEM_PROBE_BIN') and not ENV['SYSTEM_PROBE_BIN'].empty?
-    copy "pkg/ebpf/bytecode/build/*.o", "#{install_dir}/embedded/share/system-probe/ebpf/"
+    copy "pkg/ebpf/bytecode/build/*.o", "#{install_dir}/embedded/share/system-probe/ebpf/", cwd: source_path
     delete "#{install_dir}/embedded/share/system-probe/ebpf/usm_events_test*.o"
-    copy "pkg/ebpf/bytecode/build/co-re/*.o", "#{install_dir}/embedded/share/system-probe/ebpf/co-re/"
-    copy "pkg/ebpf/bytecode/build/runtime/*.c", "#{install_dir}/embedded/share/system-probe/ebpf/runtime/"
-    copy "#{ENV['SYSTEM_PROBE_BIN']}/clang-bpf", "#{install_dir}/embedded/bin/clang-bpf"
-    copy "#{ENV['SYSTEM_PROBE_BIN']}/llc-bpf", "#{install_dir}/embedded/bin/llc-bpf"
-    copy "#{ENV['SYSTEM_PROBE_BIN']}/minimized-btfs.tar.xz", "#{install_dir}/embedded/share/system-probe/ebpf/co-re/btf/minimized-btfs.tar.xz"
+    copy "pkg/ebpf/bytecode/build/co-re/*.o", "#{install_dir}/embedded/share/system-probe/ebpf/co-re/", cwd: source_path
+    copy "pkg/ebpf/bytecode/build/runtime/*.c", "#{install_dir}/embedded/share/system-probe/ebpf/runtime/", cwd: source_path
+    copy "#{ENV['SYSTEM_PROBE_BIN']}/clang-bpf", "#{install_dir}/embedded/bin/clang-bpf", cwd: source_path
+    copy "#{ENV['SYSTEM_PROBE_BIN']}/llc-bpf", "#{install_dir}/embedded/bin/llc-bpf", cwd: source_path
+    copy "#{ENV['SYSTEM_PROBE_BIN']}/minimized-btfs.tar.xz", "#{install_dir}/embedded/share/system-probe/ebpf/co-re/btf/minimized-btfs.tar.xz", cwd: source_path
   end
 
-  copy 'pkg/ebpf/c/COPYING', "#{install_dir}/embedded/share/system-probe/ebpf/"
+  copy 'pkg/ebpf/c/COPYING', "#{install_dir}/embedded/share/system-probe/ebpf/", cwd: source_path
 end
