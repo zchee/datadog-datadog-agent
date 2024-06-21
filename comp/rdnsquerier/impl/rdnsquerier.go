b52c42d504c394a06f008cac124ec9058ba4d0c5
Jim Wilson <86026167+jmw51798@users.noreply.github.com>
2024-06-21 12:52:12 -0600

    ...

--- comp/rdnsquerier/impl/rdnsquerier.go
+++ comp/rdnsquerier/impl/rdnsquerier.go
@@ -7,10 +7,10 @@
 package rdnsquerierimpl
 
 import (
-	//JMW"fmt"
-	//JMW"net"
-	//JMW"time"
 	"context"
+	"net"
+	"time"
+
 	"github.com/DataDog/datadog-agent/comp/core/config"
 	"github.com/DataDog/datadog-agent/comp/core/log"
 	compdef "github.com/DataDog/datadog-agent/comp/def"
@@ -35,6 +35,12 @@ type Provides struct {
 type rdnsQuerierImpl struct {
 	config config.Component
 	logger log.Component
+
+	// mutex for JMW
+	//JMWmutex sync.RWMutex
+
+	// map of ip to hostname and expiration time
+	cache map[string]rdnsCacheEntry
 }
 
 // NewComponent creates a new rdnsquerier component
@@ -55,31 +61,24 @@ func NewComponent(reqs Requires) (Provides, error) {
 }
 
 func (q *rdnsQuerierImpl) start(context.Context) error {
+	// JMW start workers
+	q.logger.Infof("JMWRDNSQ Starting RDNS Querier with JMW workers")
 	return nil
 }
 
 func (q *rdnsQuerierImpl) stop(context.Context) error {
+	q.logger.Infof("JMWRDNSQ Stopping RDNS Querier")
 	return nil
 }
 
-// GetHostname gets the hostname for the given IP address if the IP address is in the private address space, or returns an empty string if not.
+// GetHostname gets the hostname for the given IP address.  If the IP address is invalid or is not in the private address space then it returns an empty string.
 // The initial implementation always returns an empty string.
-func (q *rdnsQuerierImpl) GetHostname(_ []byte) string {
+func (q *rdnsQuerierImpl) GetHostnameEmtyString(_ []byte) string {
+	q.logger.Infof("JMWRDNSQ GetHostname() - returning empty string")
 	return ""
 }
 
-// JMWNEXT
-type rdnsQuerierImpl struct {
-	lc fx.Lifecycle
-	//JMWUNUSED logger log.Component
-
-	// mutex for JMW
-	//JMWmutex sync.RWMutex
-
-	// map of ip to hostname and expiration time
-	cache map[string]rdnsCacheEntry
-}
-
+// JMWNEXT-----------------------------------------------------------------------------------------------------------------------
 type rdnsCacheEntry struct {
 	//JMWhostname string
 	//JMWUNUSED expirationTime int64
@@ -87,20 +86,20 @@ type rdnsCacheEntry struct {
 	//JMWcallbacks map[string]func(string)
 }
 
-func timer(name string) func() {
+func (q *rdnsQuerierImpl) timer(name string) func() {
 	start := time.Now()
 	return func() {
-		fmt.Printf("[timer] %s: took %v usec\n", name, time.Since(start).Microseconds())
+		q.logger.Infof("[timer] %s: took %v usec\n", name, time.Since(start).Microseconds())
 	}
 }
 
 // GetHostname gets the hostname for the given IP address, if the IP address is in the private address space.
 func (q *rdnsQuerierImpl) GetHostname(ipAddr []byte) string {
-	defer timer("timer JMW GetHostname() all")()
+	defer q.timer("timer JMW GetHostname() all")()
 
 	ip := net.IP(ipAddr)
 	if !ip.IsPrivate() { // Note that IsPrivate() also returns false for invalid IP addresses
-		fmt.Printf("JMW GetHostname() IP address `%s` is not private\n", ip.String())
+		q.logger.Infof("JMW GetHostname() IP address `%s` is not private\n", ip.String())
 		// JMWTELEMETRY increment NOT private IP address counter
 		return ""
 	}
@@ -111,17 +110,17 @@ func (q *rdnsQuerierImpl) GetHostname(ipAddr []byte) string {
 	// BUT When using the host C library resolver, at most one result will be returned.
 	// So for now, when specifying DNS resolvers is not supported, if we get an error we know that there is no valid hostname returned.
 	// If/when we add support for specifying DNS resolvers, there may be multiple hostnames returned, and there may be one or more hostname returned AT THE SAME TIME an error is returned.  To keep it simple, if there is no error, we will just return the first hostname, and if there is an error, we will return an empty string and add telemetry about the error.
-	defer timer("JMW GetHostname() LookupAddr")()
+	defer q.timer("JMW GetHostname() LookupAddr")()
 	hostnames, err := net.LookupAddr(addr)
 	if err != nil {
 		//JMWADDLOGGER f.logger.Warnf("JMW Failed to lookup hostname for IP address `%s`: %s", addr, err)
-		fmt.Printf("JMW GetHostname() error looking up hostname for IP address `%s`: %s\n", addr, err)
+		q.logger.Infof("JMW GetHostname() error looking up hostname for IP address `%s`: %v\n", addr, err)
 		// JMWTELEMETRY increment metric for failed lookups - JMW should I differentiate between no match and other errors? or just tag w/ error?  how to tag w/ error w/out the tag being a string (unlimited cardinality)?
 		return ""
 	}
 
 	if len(hostnames) == 0 { // JMW is this even possible? // JMWRM?
-		fmt.Printf("JMW IP address `%s` has no match - returning empty hostname", addr)
+		q.logger.Infof("JMW IP address `%s` has no match - returning empty hostname", addr)
 		// JMWTELEMETRY increment metric for no match
 		return ""
 	}
@@ -130,7 +129,7 @@ func (q *rdnsQuerierImpl) GetHostname(ipAddr []byte) string {
 	//if (len(hostnames) > 1) {
 	// JMWTELEMETRY increment metric for multiple hostnames
 	//}
-	fmt.Printf("JMW GetHostname() IP address `%s` matched - returning hostname `%s`\n", addr, hostnames[0])
+	q.logger.Infof("JMW GetHostname() IP address `%s` matched - returning hostname `%s`\n", addr, hostnames[0])
 	return hostnames[0]
 }
 
