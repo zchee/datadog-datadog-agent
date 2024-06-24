2c828029e426042468f7969bcc1649bfccf44141
amit.slavin <amit.slavin@datadoghq.com>
2024-06-24 10:23:24 +0300

    added postgres telemetry

--- pkg/network/protocols/postgres/telemetry.go
+++ pkg/network/protocols/postgres/telemetry.go
@@ -1,24 +1,40 @@
 // Unless explicitly stated otherwise all files in this repository are licensed
 // under the Apache License Version 2.0.
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
-// Copyright 2016-present Datadog, Inc.
+// Copyright 2024-present Datadog, Inc.
 
 //go:build linux_bpf
 
 package postgres
 
 import (
+	"fmt"
+
 	libtelemetry "github.com/DataDog/datadog-agent/pkg/network/protocols/telemetry"
 	"github.com/DataDog/datadog-agent/pkg/util/log"
 )
+
+const numberOfBuckets = 4
 
 // Telemetry is a struct to hold the telemetry for the postgres protocol
 type Telemetry struct {
 	metricGroup *libtelemetry.MetricGroup
 
-	exceededQueryLength       *libtelemetry.Counter // this happens when the query length reaches capacity
-	failedTableNameExtraction *libtelemetry.Counter // this happens when we failed to extract the table name
-	failedOperationExtraction *libtelemetry.Counter // this happens when we failed to extract the operation
+	// exceededQueryLengthBuckets holds the counters for the different buckets of exceeding query length quires
+	exceededQueryLengthBuckets [numberOfBuckets]*libtelemetry.Counter
+	// failedTableNameExtraction holds the counter for the failed table name extraction
+	failedTableNameExtraction *libtelemetry.Counter
+	// failedOperationExtraction holds the counter for the failed operation extraction
+	failedOperationExtraction *libtelemetry.Counter
+}
+
+// createQueryLengthBuckets initializes the query length buckets
+func createQueryLengthBuckets(metricGroup *libtelemetry.MetricGroup) [numberOfBuckets]*libtelemetry.Counter {
+	var buckets [numberOfBuckets]*libtelemetry.Counter
+	for i := 0; i < numberOfBuckets; i++ {
+		buckets[i] = metricGroup.NewCounter("exceeded_query_length_bucket"+fmt.Sprint(i), libtelemetry.OptStatsd)
+	}
+	return buckets
 }
 
 // NewTelemetry creates a new Telemetry
@@ -26,25 +42,35 @@ func NewTelemetry() *Telemetry {
 	metricGroup := libtelemetry.NewMetricGroup("usm.postgres")
 
 	return &Telemetry{
-		metricGroup:               metricGroup,
-		exceededQueryLength:       metricGroup.NewCounter("exceeded_query_length", libtelemetry.OptStatsd),
-		failedTableNameExtraction: metricGroup.NewCounter("failed_table_name_extraction", libtelemetry.OptStatsd),
-		failedOperationExtraction: metricGroup.NewCounter("failed_operation_extraction", libtelemetry.OptStatsd),
+		metricGroup:                metricGroup,
+		exceededQueryLengthBuckets: createQueryLengthBuckets(metricGroup),
+		failedTableNameExtraction:  metricGroup.NewCounter("failed_table_name_extraction", libtelemetry.OptStatsd),
+		failedOperationExtraction:  metricGroup.NewCounter("failed_operation_extraction", libtelemetry.OptStatsd),
 	}
 }
 
-// Count increments the telemetry counters
-func (t *Telemetry) Count(tx *EbpfTx, eventWrapper *EventWrapper) {
-	if tx.Original_query_size > BufferSize {
-		t.exceededQueryLength.Add(1)
-	}
-	if eventWrapper.operation == UnknownOP {
-		t.failedOperationExtraction.Add(1)
-	}
-	if eventWrapper.tableName == "UNKNOWN" {
-		t.failedTableNameExtraction.Add(1)
+// Count increments the telemetry counters based on the event data
+func (t *Telemetry) Count(tx *EbpfEvent) {
+	eventWrapper := NewEventWrapper(tx)
+	querySize := tx.Tx.Original_query_size
+
+	switch {
+	case querySize > BufferSize*8:
+		t.exceededQueryLengthBuckets[3].Add(1)
+	case querySize > BufferSize*4:
+		t.exceededQueryLengthBuckets[2].Add(1)
+	case querySize > BufferSize*2:
+		t.exceededQueryLengthBuckets[1].Add(1)
+	case querySize > BufferSize:
+		t.exceededQueryLengthBuckets[0].Add(1)
 	}
 
+	if eventWrapper.Operation() == UnknownOP {
+		t.failedOperationExtraction.Add(1)
+	}
+	if eventWrapper.TableName() == "UNKNOWN" {
+		t.failedTableNameExtraction.Add(1)
+	}
 }
 
 // Log logs the postgres stats summary
--- pkg/network/protocols/postgres/telemetry_test.go
+++ pkg/network/protocols/postgres/telemetry_test.go
@@ -1,7 +1,7 @@
 // Unless explicitly stated otherwise all files in this repository are licensed
 // under the Apache License Version 2.0.
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
-// Copyright 2016-present Datadog, Inc.
+// Copyright 2024-present Datadog, Inc.
 
 //go:build linux_bpf
 
@@ -16,7 +16,7 @@ import (
 )
 
 type telemetryResults struct {
-	exceededQueryLength       int64
+	exceededQueryLength       [4]int64
 	failedTableNameExtraction int64
 	failedOperationExtraction int64
 }
@@ -24,58 +24,61 @@ type telemetryResults struct {
 func TestTelemetry_Count(t *testing.T) {
 	tests := []struct {
 		name              string
-		tx                *EbpfTx
-		eventWrapper      *EventWrapper
+		query             string
+		tx                []*EbpfEvent
 		expectedTelemetry telemetryResults
 	}{
 		{
-			name: "exceeded query length",
-			tx: &EbpfTx{
-				Original_query_size: 200,
+			name: "exceeded query length bucket for each bucket ones",
+			tx: []*EbpfEvent{
+				{
+					Tx: EbpfTx{
+						Original_query_size: 120,
+					},
+				},
+				{
+					Tx: EbpfTx{
+						Original_query_size: 129,
+					},
+				},
+				{
+					Tx: EbpfTx{
+						Original_query_size: 257,
+					},
+				},
+				{
+					Tx: EbpfTx{
+						Original_query_size: 514,
+					},
+				},
 			},
-			eventWrapper: &EventWrapper{
-				operation: SelectOP,
-				tableName: "table",
-			},
+
 			expectedTelemetry: telemetryResults{
-				exceededQueryLength: 1,
+				exceededQueryLength:       [4]int64{1, 1, 1, 1},
+				failedOperationExtraction: 4,
+				failedTableNameExtraction: 4,
 			},
 		},
 		{
-			name: "failed operation extraction",
-			tx: &EbpfTx{
-				Original_query_size: 100,
-			},
-			eventWrapper: &EventWrapper{
-				operation: UnknownOP,
-				tableName: "table",
-			},
+			name:  "failed operation extraction",
+			tx:    []*EbpfEvent{{}},
+			query: "CREA TABLE dummy",
 			expectedTelemetry: telemetryResults{
 				failedOperationExtraction: 1,
 			},
 		},
 		{
-			name: "failed table name extraction",
-			tx: &EbpfTx{
-				Original_query_size: 100,
-			},
-			eventWrapper: &EventWrapper{
-				operation: SelectOP,
-				tableName: "UNKNOWN",
-			},
+			name:  "failed table name extraction",
+			tx:    []*EbpfEvent{{}},
+			query: "CREATE TABLE",
 			expectedTelemetry: telemetryResults{
 				failedTableNameExtraction: 1,
 			},
 		},
 		{
-			name: "failed table name and operation extraction",
-			tx: &EbpfTx{
-				Original_query_size: 100,
-			},
-			eventWrapper: &EventWrapper{
-				operation: UnknownOP,
-				tableName: "UNKNOWN",
-			},
+			name:  "failed table name and operation extraction",
+			tx:    []*EbpfEvent{{}},
+			query: "CRE TABLE",
 			expectedTelemetry: telemetryResults{
 				failedTableNameExtraction: 1,
 				failedOperationExtraction: 1,
@@ -86,14 +89,22 @@ func TestTelemetry_Count(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			telemetry.Clear()
 			tel := NewTelemetry()
-			tel.Count(tt.tx, tt.eventWrapper)
+			if tt.query != "" {
+				tt.tx[0].Tx.Original_query_size = uint32(len(tt.query))
+				copy(tt.tx[0].Tx.Request_fragment[:], tt.query)
+			}
+			for _, tx := range tt.tx {
+				tel.Count(tx)
+			}
 			verifyTelemetry(t, tel, tt.expectedTelemetry)
 		})
 	}
 }
 
 func verifyTelemetry(t *testing.T, tel *Telemetry, expected telemetryResults) {
-	assert.Equal(t, tel.exceededQueryLength.Get(), expected.exceededQueryLength, "exceededQueryLength count is incorrect")
+	for i := 0; i < len(tel.exceededQueryLengthBuckets); i++ {
+		assert.Equal(t, tel.exceededQueryLengthBuckets[i].Get(), expected.exceededQueryLength[i], "exceededQueryLength for bucket %d count is incorrect", i)
+	}
 	assert.Equal(t, tel.failedTableNameExtraction.Get(), expected.failedTableNameExtraction, "failedTableNameExtraction count is incorrect")
 	assert.Equal(t, tel.failedOperationExtraction.Get(), expected.failedOperationExtraction, "failedOperationExtraction count is incorrect")
 }
