f7a0883a0478eb89516d92c1cd8ddce495d227f5
Kylian Serrania <kylian.serrania@datadoghq.com>
2024-09-19 15:05:08 -0400

    [gitlab] Test zip files

--- tasks/winbuildscripts/extract-modcache.bat
+++ tasks/winbuildscripts/extract-modcache.bat
@@ -16,27 +16,24 @@ if "%2" == "" (
 
 set MODCACHE_ROOT=%1
 set MODCACHE_FILE_ROOT=%2
-set MODCACHE_XZ_FILE=%MODCACHE_ROOT%\%MODCACHE_FILE_ROOT%.tar.xz
-set MODCACHE_TAR_FILE=%MODCACHE_ROOT%\%MODCACHE_FILE_ROOT%.tar
+set MODCACHE_ZIP_FILE=%MODCACHE_ROOT%\%MODCACHE_FILE_ROOT%.zip
 
 if "%GOMODCACHE%" == "" (
     @echo GOMODCACHE environment variable not set, skipping expansion of mod cache files
     goto :endofscript
 )
 
-@echo MODCACHE_XZ_FILE %MODCACHE_XZ_FILE% MODCACHE_TAR_FILE %MODCACHE_TAR_FILE% GOMODCACHE %GOMODCACHE%
-if exist %MODCACHE_XZ_FILE% (
-    @echo Extracting modcache file %MODCACHE_XZ_FILE%
-    Powershell -C "7z x %MODCACHE_XZ_FILE% -o%MODCACHE_ROOT% -bt"
-    dir %MODCACHE_TAR_FILE%
+@echo MODCACHE_ZIP_FILE %MODCACHE_ZIP_FILE% GOMODCACHE %GOMODCACHE%
+if exist %MODCACHE_ZIP_FILE% (
+    @echo Extracting modcache file %MODCACHE_ZIP_FILE%
     REM Use -aoa to allow overwriting existing files
     REM This shouldn't have any negative impact: since modules are
     REM stored per version and hash, files that get replaced will
     REM get replaced by the same files
-    Powershell -C "7z x %MODCACHE_TAR_FILE% -o%GOMODCACHE% -aoa -bt"
+    Powershell -C "7z x %MODCACHE_ZIP_FILE% -o%GOMODCACHE% -aoa -bt"
     @echo Modcache extracted
 ) else (
-    @echo %MODCACHE_XZ_FILE% not found, dependencies will be downloaded
+    @echo %MODCACHE_ZIP_FILE% not found, dependencies will be downloaded
 )
 goto :endofscript
 
@@ -46,13 +43,9 @@ goto :endofscript
 goto :eof
 
 :endofscript
-if exist %MODCACHE_XZ_FILE% (
-    @echo deleting modcache tar.xz %MODCACHE_XZ_FILE%
-    del /f %MODCACHE_XZ_FILE%
-)
-if exist %MODCACHE_TAR_FILE% (
-    @echo deleting modcache tar %MODCACHE_TAR_FILE%
-    del /f %MODCACHE_TAR_FILE%
+if exist %MODCACHE_ZIP_FILE% (
+    @echo deleting modcache zip %MODCACHE_ZIP_FILE%
+    del /f %MODCACHE_ZIP_FILE%
 )
 goto :EOF
 
