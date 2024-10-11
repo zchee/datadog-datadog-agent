DataDog/datadog-agent

Add probe for cudSetDevice

pkg/gpu/ebpf/c/gpu.c
@@ -18,6 +18,7 @@ char __license[] SEC("license") = "GPL";
 BPF_RINGBUF_MAP(cuda_events, cuda_event_header_t);
 BPF_LRU_MAP(cuda_alloc_cache, __u64, cuda_alloc_request_args_t, 1024)
 BPF_LRU_MAP(cuda_sync_cache, __u64, __u64, 1024)
+BPF_LRU_MAP(cuda_set_device_cache, __u64, __u32, 1024)
 
 // cudaLaunchKernel receives the dim3 argument by value, which gets translated as
 // a 64 bit register with the x and y values in the lower and upper 32 bits respectively,
@@ -160,6 +161,51 @@ int BPF_URETPROBE(uretprobe__cudaStreamSynchronize) {
     log_debug("cudaStreamSynchronize[ret]: EMIT cudaSync pid_tgid=%llu, stream_id=%llu", event.header.pid_tgid, event.header.stream_id);
 
     bpf_ringbuf_output(&cuda_events, &event, sizeof(event), 0);
+    bpf_map_delete_elem(&cuda_sync_cache, &pid_tgid);
+
+    return 0;
+}
+
+SEC("uprobe/cudaSetDevice")
+int BPF_UPROBE(uprobe__cudaSetDevice, __u32 device) {
+    __u64 pid_tgid = bpf_get_current_pid_tgid();
+
+    log_debug("cudaSetDevice: pid=%llu, device=%d", pid_tgid, device);
+    bpf_map_update_elem(&cuda_set_device_cache, &pid_tgid, &device, BPF_ANY);
+
+    return 0;
+}
+
+SEC("uprobe/cudaSetDevice")
+int BPF_UPROBE(uprobe__cudaSetDevice, __u32 device) {
+    __u64 pid_tgid = bpf_get_current_pid_tgid();
+    __u64 *device = NULL;
+    cuda_set_device_event_t event = { 0 };
+    __u32 retval = PT_REGS_RC(ctx);
+
+    log_debug("cudaSetDevice[ret]: pid=%llx, retval=%ld\n", pid_tgid, retval);
+
+    if (retval != 0) {
+        // Do not emit event if cudaSetDevice failed
+        goto cleanup;
+    }
+
+    device = bpf_map_lookup_elem(&cuda_sync_cache, &pid_tgid);
+    if (!device) {
+        log_debug("cudaSetDevice[ret]: failed to find cudaSetDevice request");
+        return 0;
+    }
+
+    event.header.pid_tgid = bpf_get_current_pid_tgid();
+    event.header.stream_id = (uint64_t)0;
+    event.header.type = cuda_set_device;
+    event.header.ktime_ns = bpf_ktime_get_ns();
+    event.device = device;
+
+    log_debug("cudaSetDevice: pid=%llu, device=%d", event.header.pid_tgid, device);
+    bpf_ringbuf_output(&cuda_events, &event, sizeof(event), 0);
+
+cleanup:
     bpf_map_delete_elem(&cuda_sync_cache, &pid_tgid);
 
     return 0;

zchee/datadog-datadog-agent

pkg/gpu/ebpf/c/gpu.c
@@ -18,6 +18,7 @@ char __license[] SEC("license") = "GPL";
 BPF_RINGBUF_MAP(cuda_events, cuda_event_header_t);
 BPF_LRU_MAP(cuda_alloc_cache, __u64, cuda_alloc_request_args_t, 1024)
 BPF_LRU_MAP(cuda_sync_cache, __u64, __u64, 1024)
+BPF_LRU_MAP(cuda_set_device_cache, __u64, int, 1024)
 
 // cudaLaunchKernel receives the dim3 argument by value, which gets translated as
 // a 64 bit register with the x and y values in the lower and upper 32 bits respectively,
@@ -160,6 +161,51 @@ int BPF_URETPROBE(uretprobe__cudaStreamSynchronize) {
     log_debug("cudaStreamSynchronize[ret]: EMIT cudaSync pid_tgid=%llu, stream_id=%llu", event.header.pid_tgid, event.header.stream_id);
 
     bpf_ringbuf_output(&cuda_events, &event, sizeof(event), 0);
+    bpf_map_delete_elem(&cuda_sync_cache, &pid_tgid);
+
+    return 0;
+}
+
+SEC("uprobe/cudaSetDevice")
+int BPF_UPROBE(uprobe__cudaSetDevice, int device) {
+    __u64 pid_tgid = bpf_get_current_pid_tgid();
+
+    log_debug("cudaSetDevice: pid_tgid=%llu, device=%u", pid_tgid, device);
+    bpf_map_update_elem(&cuda_set_device_cache, &pid_tgid, &device, BPF_ANY);
+
+    return 0;
+}
+
+SEC("uretprobe/cudaSetDevice")
+int BPF_URETPROBE(uretprobe__cudaSetDevice) {
+    __u64 pid_tgid = bpf_get_current_pid_tgid();
+    int *device = NULL;
+    cuda_set_device_event_t event = { 0 };
+    __u32 retval = PT_REGS_RC(ctx);
+
+    log_debug("cudaSetDevice[ret]: pid_tgid=%llu, retval=%d\n", pid_tgid, retval);
+
+    if (retval != 0) {
+        // Do not emit event if cudaSetDevice failed
+        goto cleanup;
+    }
+
+    device = bpf_map_lookup_elem(&cuda_set_device_cache, &pid_tgid);
+    if (!device) {
+        log_debug("cudaSetDevice[ret]: failed to find cudaSetDevice request");
+        return 0;
+    }
+
+    event.header.pid_tgid = bpf_get_current_pid_tgid();
+    event.header.stream_id = (uint64_t)0;
+    event.header.type = cuda_set_device;
+    event.header.ktime_ns = bpf_ktime_get_ns();
+    event.device = *device;
+
+    log_debug("cudaSetDevice: EMIT pid_tgid=%llu, device=%d", event.header.pid_tgid, *device);
+    bpf_ringbuf_output(&cuda_events, &event, sizeof(event), 0);
+
+cleanup:
     bpf_map_delete_elem(&cuda_sync_cache, &pid_tgid);
 
     return 0;